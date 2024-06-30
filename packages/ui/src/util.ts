75dd9f45c2ff9b6e1818eda143a74b361f36d326
Dave Dennis <dave.w.dennis@gmail.com>
2024-06-29 19:03:56 -0500

    feat: remark seems to be working

--- packages/ui/src/util.ts
+++ packages/ui/src/util.ts
@@ -1,12 +1,16 @@
 import { MDXRemoteSerializeResult } from "next-mdx-remote";
 import { serialize } from "next-mdx-remote/serialize";
+import remarkGfm from 'remark-gfm';
 
 export async function fetchGitMdxData(slug: string): Promise<MDXRemoteSerializeResult> {
     const res = await fetch(
-      `https://raw.githubusercontent.com/DaveVED/my-posts/master/posts/${slug}.mdx`,
+        `https://raw.githubusercontent.com/DaveVED/my-posts/master/posts/${slug}.mdx`,
     );
     const mdxText = await res.text();
-    const mdxSource = await serialize(mdxText, { parseFrontmatter: true });
+    const mdxSource = await serialize(mdxText, {
+        mdxOptions: {
+            remarkPlugins: [remarkGfm],
+        },
+    });
     return mdxSource;
-  }
-  
+}
