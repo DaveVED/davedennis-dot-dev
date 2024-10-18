b432585dc445003a87a95849aa2587ed044a5ec3
DaveVED <dave.w.dennis@gmail.com>
2024-10-18 18:04:22 -0500

    feat: added in the web pacakge now with bun

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,16 +1,10 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import App from "./app";
+import App from "./app/App.tsx";
 import "./index.css";
 
-const el = document.getElementById("root");
-if (el) {
-  const root = createRoot(el);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>,
-  );
-} else {
-  throw new Error("Could not find root element");
-}
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
