19214cd4638f7961c1dbf8923924eed8a0cfd4a9
DaveVED <dave.dennis@gs.com>
2024-11-09 15:09:24 -0600

    feat: update a few more styles, come back later

--- packages/web/src/components/blog-post.tsx
+++ packages/web/src/components/blog-post.tsx
@@ -1,10 +1,11 @@
 import {
-    useParams
+    useParams,
   } from "react-router-dom";
 import DynamicMdx from "./dynamic-mdx";
 import frontMatter from 'front-matter';
 import { useEffect, useState } from "react";
-
+import  { formatDate } from "@/lib/utils";
+import daveCaveAvatar from "../assets/dave-cave-avatar.png"
 export const BlogPost = () => {
     const [post, setPost] = useState<any>(null);
     let { postId } = useParams();
@@ -38,9 +39,52 @@ export const BlogPost = () => {
         return <div>Loading...</div>;
       }
     return (
-      <div>
-        <h3>ID: {postId}</h3>
-        <DynamicMdx>{post.body}</DynamicMdx>
+        <article className="container relative max-w-3xl py-6 lg:py-10">
+    
+    <div>
+        {post.date && (
+          <time
+            dateTime={post.date}
+            className="block text-sm text-muted-foreground"
+          >
+            Published on {formatDate(post.date)}
+          </time>
+        )}
+        <h1 className="mt-2 inline-block font-heading text-4xl leading-tight lg:text-5xl">
+          {post.title}
+        </h1>
+        <div className="mt-4 flex space-x-4">
+          <a
+            href={`https://twitter.com/DaveVED_`}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="flex items-center space-x-2 text-sm"
+          >
+            <img
+              src={daveCaveAvatar}
+              alt={"Dave Dennis"}
+              width={42}
+              height={42}
+              className="rounded-full bg-white"
+            />
+            <div className="flex-1 text-left leading-tight">
+              <p className="font-medium">{"Dave Dennis"}</p>
+              <p className="text-[12px] text-muted-foreground">
+                @{"DaveVED_"}
+              </p>
+            </div>
+          </a>
+        </div>
       </div>
+        <img
+          src={post.image}
+          alt={post.title}
+          width={720}
+          height={405}
+          className="my-8 rounded-md border bg-muted transition-colors"
+        />
+        {/* Render the MDX content */}
+        <DynamicMdx>{post.body}</DynamicMdx>
+      </article>
     );
 }
